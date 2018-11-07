e0bfc597cba36416d6eb3f0960cd7ce4acb77e60
harry10148 <harry10148@gmail.com>
2018-11-07 13:23:27 +0800

    aws update

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Logging in to AWS ECR and pull images"
-$(aws ecr get-login --no-include-email --region ap-northeast-1)
+$(aws ecr get-login --no-include-email --registry-ids 204065533127 --region ap-northeast-1)
 docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
 docker pull mysql:latest
 docker pull redis:latest
