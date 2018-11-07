4217de71ca64c35b05b995343ab4f7a92326f4f7
harry10148 <harry10148@gmail.com>
2018-11-07 12:57:35 +0800

    aws1

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,7 +1,11 @@
 
 #!/bin/bash
+docker ps > /home/ec2-user/container_log
+containerNum=cat /home/ec2-user/container_log|wc -l
+if [ $containerNum  > 1 ];then
 echo "stop service container "
 cd /home/ec2-user/deploy
 echo "remove container "
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+fi
