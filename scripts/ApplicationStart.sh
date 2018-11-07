f4d0098d51b2866b129217d31a6ef464740c4e67
harry10148 <harry10148@gmail.com>
2018-11-07 20:59:15 +0800

    aws update

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -5,13 +5,14 @@ echo "Running service container "
 docker-compose up -d
 echo "Testing service container "
 touch /home/ec2-user/container_log
+touch /home/ec2-user/start_log
 docker ps > /home/ec2-user/container_log
 containerNum=cat /home/ec2-user/container_log|wc -l
 for ((i=0;i<3;i++))
 do  
   sleep 5
   if [ $containerNum == 6 ];then
-    echo "service deploy success !!"
+    echo "service deploy success !!" > /home/ec2-user/start_log
     break
 fi
   if [ $i == 2 ];then
@@ -20,6 +21,6 @@ fi
   docker rmi mysql:latest
   docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
   docker rmi 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
-  echo "service deploy fail"
+  echo "service deploy fail" > /home/ec2-user/start_log
 fi
 done
