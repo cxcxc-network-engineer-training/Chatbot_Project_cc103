63c50806418b817ebc66723f3bb108ecd83baebe
harry10148 <harry10148@gmail.com>
2018-11-09 00:46:44 +0800

    aws

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -2,4 +2,10 @@
 echo "stop service container "
 cd /home/ec2-user/deploy
 echo "remove container & images"
+touch /home/ec2-user/redeploy
+repo=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
+docker images | grep $repo > /home/ec2-user/redeploy
+rescan=cat /home/ec2-user/redeploy|wc -l
+if [ $rescan > 1 ];then
 docker-compose down --rmi all
+fi
