eac8d54a89f0c398e1620037b3818811d208db83
harry10148 <harry10148@gmail.com>
2018-11-08 17:45:41 +0800

    aws update 11/08

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-docker ps > /home/ec2-user/container_log
-containerNum=cat /home/ec2-user/container_log|wc -l
-if [ $containerNum  > 1 ];then
 echo "stop service container "
 cd /home/ec2-user/deploy
-echo "remove container "
-docker stop $(docker ps -a -q)
-docker rm -f $(docker ps -a -q)
-echo "remove images "
-docker rmi -f $(docker images -a -q)
-fi
+echo "remove container & images"
+docker-compose down --rmi all
