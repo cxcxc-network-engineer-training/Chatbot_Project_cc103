cf864e788dbeb44c73d37701205c8fc811679091
harry10148 <harry10148@gmail.com>
2018-11-08 19:45:34 +0800

    a

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
- chmod +x /usr/local/bin/docker-compose
- ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-yum install -y awslogs
-sed -i 's/us-east-1/ap-northeast-1/g' /etc/awslogs/awscli.conf
-sed -i 's/\/var\/log\/messages/\/home\/ec2-user\/Chatbot_Project_cc103\/Chatbot_Dev_cc103\/code\/my.log/g' /etc/awslogs/awslogs.conf
-service awslogs start
+sudo yum install -y awslogs
+sudo sed -i 's/us-east-1/ap-northeast-1/g' /etc/awslogs/awscli.conf
+sudo sed -i 's/\/var\/log\/messages/\/home\/ec2-user\/Chatbot_Project_cc103\/Chatbot_Dev_cc103\/code\/my.log/g' /etc/awslogs/awslogs.conf
+sudo service awslogs start
