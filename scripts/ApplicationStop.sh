5708f58cffeefdb9c2c1dc615cf3c3d3dab22725
harry10148 <harry10148@gmail.com>
2018-11-09 10:40:00 +0800

    aa

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 echo "stop service container "
 cd /home/ec2-user/deploy
-touch /home/ec2-user/redeploy
-repo=204065533127.dkr.ecr.ap-northeast-1.amazonaws.com
-docker images | grep $repo > /home/ec2-user/redeploy
-rescan=cat /home/ec2-user/redeploy|wc -l
-if [ $rescan > 1 ];then
 docker-compose down --rmi all
-fi
 exit 0
--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Logging in to AWS ECR and pull images"
+
 $(aws ecr get-login --no-include-email --region ap-northeast-1)
-docker push 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
-docker push 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
-docker push 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/api:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/jupyter:latest
+docker pull 204065533127.dkr.ecr.ap-northeast-1.amazonaws.com/ngrok:latest
 docker pull mysql:latest
 docker pull redis:latest
 exit 0
